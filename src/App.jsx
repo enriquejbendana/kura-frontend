6aa0956cf69f262a00ac63a2f21921a42fbcbcf5
Enrique <enriquejbendana@hotmail.com>
2026-06-15 08:45:16 -0300

    Fix JSX syntax error that caused build failure

--- src/App.jsx
+++ src/App.jsx
@@ -806,7 +806,6 @@ function App() {
                                   </span>
                                 )}
                               </div>
-                              </div>
                               <div className="row-price-info">
                                 {isBestPrice && (
                                   <svg className="best-price-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor" stroke="none"><polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"/></svg>
